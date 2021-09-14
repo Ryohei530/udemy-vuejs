a5b34112a35ede240444b312711354833bc1173a
Ryohei530 <kuboryo777@gmail.com>
2021-09-14 18:08:28 +0900

    first commit

---
+++ src/components/About.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <p>About</p>
+    <input type="text" name=""/>
+  </div>
+</template>
---
+++ src/components/EventTitle.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <label for="title">タイトル</label>
+    <input 
+      id="title" 
+      type="text"
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    />
+    <pre>{{ value }}</pre>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: ["value"]
+  };
+</script>
